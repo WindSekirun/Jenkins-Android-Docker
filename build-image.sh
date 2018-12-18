b6e23d89e39bd2f1ac3c51930dd0ce247d10825e
Pyxis <pyxis@uzuki.live>
2018-12-18 13:23:18 +0000

    add runImage.sh

--- build-image.sh
+++ build-image.sh
@@ -1,1 +1,3 @@
-docker build -t jenkins-android-docker .
+docker build -t jenkins-android-docker:1.0.1 .
+docker tag jenkins-android-docker:1.0.1 windsekirun/jenkins-android-docker:1.0.1
+docker push windsekirun/jenkins-android-docker:1.0.1
