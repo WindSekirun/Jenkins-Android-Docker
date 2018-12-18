e180628f9e95cb2a4d374c031f927e194714e298
Pyxis <pyxis@uzuki.live>
2018-12-18 14:33:06 +0000

    add runImage2500.sh

---
+++ runImage2500.sh
@@ -0,0 +1,3 @@
+mkdir /data/jenkins-android-docker
+sudo chown -R 1000:1000 /data/jenkins-android-docker
+sudo docker run -d -m 2500m -p 8080:8080 -p 50000:50000 -v /data/jenkins-android-docker:/var/jenkins_home windsekirun/jenkins-android-docker:1.0.1
