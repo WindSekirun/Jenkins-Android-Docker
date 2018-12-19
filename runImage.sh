1e698f8ec5a7873b909ed71fbdd201d10e997910
Pyxis <pyxis@uzuki.live>
2018-12-19 14:58:45 +0000

    add name

--- runImage.sh
+++ runImage.sh
@@ -1,3 +1,3 @@
 mkdir /data/jenkins-android-docker
 sudo chown -R 1000:1000 /data/jenkins-android-docker
-sudo docker run -d -p 8080:8080 -p 50000:50000 -v /data/jenkins-android-docker:/var/jenkins_home windsekirun/jenkins-android-docker:1.0.1
+sudo docker run -d --name jenkins -p 8080:8080 -p 50000:50000 -v /data/jenkins-android-docker:/var/jenkins_home windsekirun/jenkins-android-docker:1.0.2
