51b84fdee795444327ffbb72a42accc54c37c142
WindSekirun (DongGil, Seo) <pyxis@uzuki.live>
2018-12-31 14:24:19 +0900

    Delete runImage2500.sh

--- runImage2500.sh
+++
@@ -1,3 +0,0 @@
-mkdir /data/jenkins-android-docker
-sudo chown -R 1000:1000 /data/jenkins-android-docker
-sudo docker run -d --name jenkins -m 2500m -p 8080:8080 -p 50000:50000 -v /data/jenkins-android-docker:/var/jenkins_home windsekirun/jenkins-android-docker:1.0.2
