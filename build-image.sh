bee77a3c19bf6524531981a509c60d143cfdb0b5
WindSekirun (DongGil, Seo) <pyxis@uzuki.live>
2018-12-31 14:25:07 +0900

    Delete build-image.sh

--- build-image.sh
+++
@@ -1,3 +0,0 @@
-docker build -t jenkins-android-docker:1.0.1 .
-docker tag jenkins-android-docker:1.0.1 windsekirun/jenkins-android-docker:1.0.1
-docker push windsekirun/jenkins-android-docker:1.0.1
